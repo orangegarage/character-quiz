2d310f411bc17ca4ccc4179b13e480acb312fb4d
Jin Jeong <jinjeong@microsoft.com>
2022-06-14 18:07:03 -0400

    adjust margin and move divs so my face icon is a little more to left

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,15 @@ import reportWebVitals from "./reportWebVitals";
 import jinLogo from "./jinhub.webp";
 
 const navElement = (
-  <div className = "nav">
-    <nav className="flex flex-nowrap items-center justify between p-6 container mx-auto">
-      <a href = "https://github.com/orangegarage" target ="_blank" rel="noreferrer" className = "mr-10">
+  <div className = "bg-[#282c34] flex items-center">
+    <nav className="flex items-center p-6 mx-auto">
+      <a href = "https://github.com/orangegarage" target ="_blank" rel="noreferrer" className = "lg:mr-10 mr-5">
         <img src={jinLogo} alt="jinlogo" className= "h-10 w-10"></img>
       </a>
-      <div className = "text-#61dafb text-lg lg:flex">
-        <Link to="/" className = "block mt-4 lg:inline-block lg:mt-0 mr-10">Home</Link>
-        <Link to="/Quiz" className = "block mt-4 lg:inline-block lg:mt-0 mr-10">Quiz</Link>
-        <Link to="/All" className = "block mt-4 lg:inline-block lg:mt-0 mr-10">View All</Link>
+      <div className = "text-#61dafb text-lg flex justify-items-center">
+        <Link to="/" className = "block lg:inline-block lg:mt-0 mr-10 mr-5">Home</Link>
+        <Link to="/Quiz" className = "block lg:inline-block lg:mt-0 mr-5">Quiz</Link>
+        <Link to="/All" className = "block lg:inline-block lg:mt-0 mr-5">View All</Link>
       </div>
     </nav>
   </div>
