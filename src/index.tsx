22c410a3e23447735b5792ca42712ea95a475dcb
Jin <jjjinyong@naver.com>
2022-06-16 08:45:49 -0400

    minor navbar changes

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,15 @@ import reportWebVitals from "./reportWebVitals";
 import jinLogo from "./jinhub.webp";
 
 const navElement = (
-  <div className = "bg-[#282c34] flex items-center">
-    <nav className="flex items-center p-6 mx-auto">
+  <div className = "bg-[#0583D2] flex items-center">
+    <nav className="flex items-center p-4 mx-auto">
       <a href = "https://github.com/orangegarage" target ="_blank" rel="noreferrer" className = "lg:mr-10 mr-5">
         <img src={jinLogo} alt="jinlogo" className= "h-10 w-10"></img>
       </a>
       <div className = "text-#61dafb text-lg flex justify-items-center">
-        <Link to="/" className = "block lg:inline-block lg:mt-0 mr-10 mr-5">Home</Link>
-        <Link to="/Quiz" className = "block lg:inline-block lg:mt-0 mr-5">Quiz</Link>
-        <Link to="/All" className = "block lg:inline-block lg:mt-0 mr-5">View All</Link>
+        <Link to="/" className = "block lg:inline-block lg:mr-7 mr-2 font-bold px-3 py-2 rounded-md hover:text-white hover:bg-[#055e96]">Home</Link>
+        <Link to="/Quiz" className = "block lg:inline-block lg:mr-7 mr-2 font-bold px-3 py-2 rounded-md hover:text-white hover:bg-[#055e96]">Quiz</Link>
+        <Link to="/All" className = "block lg:inline-block lg:mr-7 mr-2 font-bold px-3 py-2 rounded-md hover:text-white hover:bg-[#055e96]">View All</Link>
       </div>
     </nav>
   </div>
