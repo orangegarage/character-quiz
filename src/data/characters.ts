940b38725ffaaba2ba2de0102986d75a177aab59
Jin Jeong <jinjeong@microsoft.com>
2022-06-16 15:00:19 -0400

    click now returns modal with correct character, thumbnail flexbox applied

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -10,12 +10,12 @@ export let characters = [
     number: 2,
     name: "char2-name-sample",
     image: charB,
-    answers: ["trait-1", "trait-2", "trait-5", "trait-6"],
+    traits: ["trait-1", "trait-2", "trait-5", "trait-6"],
   },
   {
     number: 3,
     name: "char3-name-sample",
     image: charC,
-    answers: ["trait-5", "trait-6", "trait-7", "trait-8"],
+    traits: ["trait-5", "trait-6", "trait-7", "trait-8"],
   },
 ];
--- src/routes/All.tsx
+++ src/routes/All.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { characters } from "../data/characters";
+import "../App.css";
 
 function AllCharacters() {
+  let [index, setIndex] = useState(-1);
+
   function characterClick(characterName: string) {
-    console.log("clicked: " + characterName);
-    //go into characters and align the name , print index related
+    for (let i: number = 0; i < characters.length; i++) {
+      if (characters[i].name === characterName) {
+        console.log("clicked: " + characterName);
+        setIndex(i);
+        return index;
+      }
+    }
   }
 
   let characterList = characters.map((character) => {
@@ -18,15 +26,90 @@ function AllCharacters() {
     );
   });
 
+  function selectedCharacter(index: number) {
+    if (index === -1) {
+      return <p className="hidden">Keep it nothing </p>;
+    } else {
+      console.log(index);
+      return (
+        <div
+          id="defaultModal"
+          tabIndex={-1}
+          aria-hidden="true"
+          className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full"
+        >
+          <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
+            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+              <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
+                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  {characters[index].name}
+                </h3>
+                <button
+                  type="button"
+                  className="exitbutton text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-toggle="defaultModal"
+                >
+                  <svg
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+
+              <div className="p-6 space-y-6">
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  {characters[index].traits}
+                </p>
+                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                  blah blah flavortext
+                </p>
+              </div>
+              <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+                <button
+                  data-modal-toggle="defaultModal"
+                  type="button"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Overview
+                </button>
+                <button
+                  data-modal-toggle="defaultModal"
+                  type="button"
+                  className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                >
+                  Build Guide
+                </button>
+              </div>
+            </div>
+          </div>
+          <p>
+            
+          </p>
+        </div>
+      );
+    }
+  }
+
+  function toggleVisibility() {
+    //toggle visibility by changing index to -1, as it is negative 1 when nothing is selected
+    setIndex(-1);
+  }
+
   return (
     <div>
       <div className="characters">
-        <ul className="characterList">{characterList}</ul>
+        <ul className="characterList flex flex-wrap">{characterList}</ul>
       </div>
-      <div className="characterInfo">
-        <p>
-          {characters[0].name}: {characters[0].traits}
-        </p>
+      <div className="characterCard" onClick={toggleVisibility}>
+        {selectedCharacter(index)}
       </div>
     </div>
   );
