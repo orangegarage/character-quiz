066489ea59c7b46b84cc40591dbdeaca0d0a5746
Jin <jjjinyong@naver.com>
2022-06-17 13:02:28 -0400

    arrows for quiz

--- src/routes/Quiz.tsx
+++ src/routes/Quiz.tsx
@@ -30,15 +30,25 @@ function Quiz() {
   let listAnswers = answers.map((answer) => <li key={answer}>{answer}</li>);
 
   return (
-    <div>
-      <div className="quizDiv text-[#61dafb]">
-        <p>{currentQuestion} out of {questions.length}</p>
-        <p>{questions[currentQuestion].question}</p>
-        <ul>{listAnswers}</ul>
+    <div className = "quizDiv flex w-1/2 mx-auto sm:my-32 my-5 items-center justify-center">
+      <div className="navButtonDiv text-white mx-10">
+        <button onClick={previousQuestion}>
+          <svg className="lg:w-20 lg:h-20 md:w-10 md:h-10 text-white w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path>
+          </svg>
+        </button>
       </div>
-      <div className="navButtonDiv text-white">
-        <button onClick={previousQuestion}>&lt;Previous</button>
-        <button onClick={nextQuestion}>Next&gt;</button>
+        <div className="carouselDiv text-white">
+          <p>{currentQuestion} out of {questions.length}</p>
+          <p>{questions[currentQuestion].question}</p>
+          <ul>{listAnswers}</ul>
+        </div>
+      <div className="navButtonDiv text-white mx-10">
+        <button onClick={nextQuestion}>
+          <svg className="lg:w-20 lg:h-20 md:w-10 md:h-10 w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
